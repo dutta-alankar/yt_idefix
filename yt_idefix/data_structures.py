080bcd28d1359e627645ba8e813fd6e5048b0eb0
Alankar Dutta <dutta.alankar@gmail.com>
2023-02-23 22:56:01 +0530

    dummy _get_field_offset_index
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- yt_idefix/data_structures.py
+++ yt_idefix/data_structures.py
@@ -245,9 +245,8 @@ class IdefixDmpHierarchy(IdefixVtkHierarchy):
 
 class PlutoXdmfHierarchy(IdefixHierarchy):
     def _get_field_offset_index(self) -> dict[str, int]:
-        retv: dict[str, int] = {}
-        retv["dummy"] = 0
-        return retv  # Dummy
+        # TODO(clm): refactor class hierarchy so this unused method can be removed
+        return {}
 
     def _detect_output_fields(self):
         with h5py.File(self.index_filename, mode="r") as h5f:
